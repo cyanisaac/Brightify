9ae9092f77bf679e257f62c0c4331c1d6d4a68c4
Isaac Trimble-Pederson <cyginnovations@gmail.com>
2017-03-31 10:18:58 -0700

    Initial preferences bundle
    Initial preference bundle from template, todo replace with my own content

---
+++ brightify/brightifypreferencesbundle/BTFYRootListController.m
@@ -0,0 +1,13 @@
+#include "BTFYRootListController.h"
+
+@implementation BTFYRootListController
+
+- (NSArray *)specifiers {
+	if (!_specifiers) {
+		_specifiers = [[self loadSpecifiersFromPlistName:@"Root" target:self] retain];
+	}
+
+	return _specifiers;
+}
+
+@end
