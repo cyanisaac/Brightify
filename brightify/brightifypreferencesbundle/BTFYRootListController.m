6c6988d7bacc1d3bde93896e21e2a0e0e20afd3e
Isaac Trimble-Pederson <cyginnovations@gmail.com>
2017-04-01 00:09:31 -0700

    Added preferences bundle

--- brightify/brightifypreferencesbundle/BTFYRootListController.m
+++ brightify/brightifypreferencesbundle/BTFYRootListController.m
@@ -10,4 +10,36 @@
 	return _specifiers;
 }
 
+- (id)tableView:(id)tableView viewForHeaderInSection:(NSInteger)section {
+	if (section == 0) {
+		// add table header
+		UIImage *logoImage = [UIImage imageWithContentsOfFile:@"/Library/PreferenceBundles/BrightifyPreferencesBundle.bundle/header.png"];
+		UIImageView *logoView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 320, 120.0)];
+		logoView.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleRightMargin;
+		logoView.image = logoImage;
+		UIView *headerView = [[UIView alloc] initWithFrame:logoView.frame];
+		headerView.backgroundColor = [UIColor whiteColor];
+		[headerView addSubview:logoView];
+		return headerView;
+	} else {
+		return [super tableView:tableView viewForHeaderInSection:section];
+	}
+}
+
+- (CGFloat)tableView:(id)tableView heightForHeaderInSection:(NSInteger)section {
+	if (section == 0) {
+		return 120.0;
+	} else {
+		return [super tableView:tableView heightForHeaderInSection:section];
+	}
+}
+
+-(void)safariTwitter {
+	[[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://twitter.com/cyanisaac"]];
+}
+
+-(void)safariRepository {
+	[[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://github.com/cyanisaac/Brightify"]];
+}
+
 @end
